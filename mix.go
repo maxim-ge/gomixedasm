cc15af1a01a3b761303fddce6ad96a9ccd0f73b1
maxim-ge <maxim.ge@gmail.com>
2022-08-20 21:22:23 +0300

    misc

--- mix.go
+++ mix.go
@@ -18,17 +18,28 @@ import (
 	"strconv"
 )
 
+/*
+Resources:
+- https://regex101.com/
+- https://regexr.com/
+*/
+
 func Mix(r io.Reader, w io.Writer) {
 	scanner := bufio.NewScanner(r)
 
 	//TEXT main.addr_arithm(SB) E:/workspaces/cloud/inv-wasm/asm/addr_arithm.go
-	var rTextObjdump = regexp.MustCompile(`(?m)TEXT (.*?)\.([^\(]*?)\(.*?\) (.*)`)
+	var rTextObjdump = regexp.MustCompile(`TEXT (.*?)\.([^\(]*)\(.*?\) (.*)`)
 
 	//	callfunc.go:5		0x4bd340	65488b0c2528000000	mov rcx, qword ptr gs:[0x28]
-	var rAsmObjdump = regexp.MustCompile(`(?m)(?m)(?m)\s*([^:]*?):-?(\d*)\s*([^\s]*)\s*([^\s]*)\s*(\S.*)`)
+	var rAsmObjdump = regexp.MustCompile(`\s*([^:]*?):-?(\d*)\s*([^\s]*)\s*([^\s]*)\s*(\S.*)`)
 
-	// //	0x0000 00000 (bytewriter_test.go:42)	TEXT	"".newLongSql(SB), ABIInternal, $24-0
-	// var rTextCompile = regexp.MustCompile(`\s(\w*)\s(\w*)\(`)
+	//	0x0000 00000 (bytewriter_test.go:42)	TEXT	"".newLongSql(SB), ABIInternal, $24-0
+	// 0x0000
+	// 00000
+	// bytewriter_test.go
+	// 42
+	// TEXT`, `"".newLongSql, ABIInternal, $24-0`
+	var rTextCompile = regexp.MustCompile(`\s(\w*)\s(\w*)\s\(([^:]*):([^\)]*)\)\s(.*)`)
 
 	var textLines []string
 	var textFileName string
@@ -45,6 +56,19 @@ func Mix(r io.Reader, w io.Writer) {
 			matches = rTextObjdump.FindStringSubmatch(str)
 			if len(matches) > 0 {
 				//funcName := matches[2]
+				fPath := matches[3]
+				_, textFileName = filepath.Split(fPath)
+				textLines = readLines(fPath)
+				lastPrintedLineNo = -1
+				fmt.Fprintf(w, ";***** %v\n", matches[0])
+				continue
+			}
+		}
+
+		//	0x0000 00000 (bytewriter_test.go:42)	TEXT	"".newLongSql(SB), ABIInternal, $24-0
+		{
+			matches = rTextCompile.FindStringSubmatch(str)
+			if len(matches) > 0 {
 				fPath := matches[3]
 				_, textFileName = filepath.Split(fPath)
 				textLines = readLines(fPath)
